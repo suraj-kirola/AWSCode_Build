fbaf22da2676954131f2b18649a45e9a425963c3
surakK <example@gmail.com>
2024-08-02 20:40:36 +0530

    Wrote code deploy agent script

---
+++ CodeDeployAgent.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e 
+
+
+# This is a script to download a CodeDeploy agent on an EC2 instance. 
+
+sudo apt update
+
+sudo apt install ruby-full -y
+
+sudo apt install ruby-full
+
+wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
+
+chmod +x ./install
+
+sudo ./install auto
+
+systemctl status codedeploy-agent
+
+
